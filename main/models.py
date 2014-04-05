ca35f4469d64119c640b2b39db8652a02450bb9f
Chris Ovenden <chris.ovenden@gmail.com>
2014-04-05 18:31:48 +0100

    set model; string repr

--- main/models.py
+++ main/models.py
@@ -11,11 +11,29 @@ FORMAT_CHOICES = (
     ('Cube', 'u'),
 )
 
+class Set(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=3)
+    
+    def __unicode__(self):
+        return self.name
+
 class Card(models.Model):
     name = models.CharField(max_length=1024)
+    set = models.ForeignKey(Set)
+    others = models.ManyToManyField('Card', blank=True, null=True)
+    
+    def __unicode__(self):
+        if len(self.others.all()) > 0:
+            return '%s (%s)' % (self.name, self.set)
+        else:
+            return self.name
     
 class Person(models.Model):
     name = models.CharField(max_length=100)
+    
+    def __unicode__(self):
+        return self.name
     
 class CardMap(models.Model):
     location = models.ForeignKey('Location')
@@ -24,10 +42,16 @@ class CardMap(models.Model):
     is_proxy = models.BooleanField(default=False)
     is_foil = models.BooleanField(default=False)
     
+    def __unicode__(self):
+        return '%s\'s %s in %s' % (self.owner, self.card, self.location)
+    
 class Location(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     cards = models.ManyToManyField(Card, through=CardMap)
     owner = models.ForeignKey(Person)
     format = models.CharField(choices=FORMAT_CHOICES, max_length=1)
+    
+    def __unicode__(self):
+        return self.name
     
