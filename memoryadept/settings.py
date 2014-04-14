8b5c46b8f196a51706ca6ef7f7cb55e470bbb2a4
Chris Ovenden <chris.ovenden@gmail.com>
2014-04-14 08:53:18 +0100

    moving to MySQL

--- memoryadept/settings.py
+++ memoryadept/settings.py
@@ -60,8 +60,10 @@ WSGI_APPLICATION = 'memoryadept.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', #'django.db.backends.sqlite3',
+        'NAME': 'memoryadept', #os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'root',
+        'PASSSWORD': '9while9',
     }
 }
 
