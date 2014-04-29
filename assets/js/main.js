5b725896e50414869a6a4a650d43abf1f580d5bb
Chris Ovenden <chris.ovenden@gmail.com>
2014-04-29 19:10:35 +0100

    extra rows on edit table

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 "use strict";
-var cardList;
+var cardList,
+    autocompleteSettings = {
+      serviceUrl: '/suggestions/',
+      //lookup: cardList,
+      onSelect: function(suggestion) {
+        console.log('selected: ' + suggestion.value + ', ' + suggestion.data);
+      }
+    };
 $('#create-new').on('submit', function(e) {
   e.preventDefault();
   $.ajax({
@@ -13,7 +20,7 @@ $('#create-new').on('submit', function(e) {
         if (json.new === true) {
           // add to list
           var newHtml = '<input type="radio" name="chosen_location" id="cb' + json.location_id + '">';
-          newHtml += '<label for="cb' + json.location_id + '">' + json.location_name + '</label>'
+          newHtml += '<label for="cb' + json.location_id + '">' + json.location_name + '</label>';
           $('#locations').append($(newHtml));
         } 
         $('#locations #cb' + json.location_id).prop('checked', true);
@@ -29,7 +36,7 @@ $('#create-new').on('submit', function(e) {
 $('#locations input[name=chosen_location]').on('change', function() {
   // load cards
   var locationId = this.id.substr(2), // slice off "cb" prefix
-      $table = $('#edit-card-list'),
+      $table = $('#edit-card-list table'),
       $rowTemplate = $table.find('tr:last-child');
   $.ajax({
     url: '/location-contents/' + locationId,
@@ -42,13 +49,29 @@ $('#locations input[name=chosen_location]').on('change', function() {
   });
 });
 
-$('.autocomplete').autocomplete({
-   serviceUrl: '/suggestions/',
-   //lookup: cardList,
-   onSelect: function(suggestion) {
-     console.log('selected: ' + suggestion.value + ', ' + suggestion.data);
-   }
+$('#edit-card-list').on('click', '.addRow',function(e) {
+  e.preventDefault();
+  addTableRow();
+  $(this).remove();
+}).on('click', '.deleteRow',function(e) {
+  e.preventDefault();
+  $(this).closest('tr').remove();
 });
+
+$('.autocomplete').autocomplete(autocompleteSettings);
+
+function addTableRow() {
+  var $table = $('#edit-card-list table'),
+      $rows = $table.find('tr:not(:first-child)'), // exclude heading
+      $rowTemplate = $table.find('tr:last-child').clone(false),
+      $newRow = $table.append($rowTemplate).find('tr:last-child'),
+      $inputs = $newRow.find('input');
+      $inputs.get(0).name = 'n' + $rows.length;
+      $inputs.get(1).name = 'c' + $rows.length;
+      
+      // attach new Autocomplete() 
+      $inputs.eq(1).autocomplete(autocompleteSettings).val('');
+}
 
 $(document).ready(function() {
     $.ajax({
